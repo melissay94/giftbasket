bf3b74e9b664b022bfbc95f9f658431862786d0a
melissay94 <melissay94@yahoo.com>
2020-04-22 15:01:16 -0700

    Set up build scripts

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
