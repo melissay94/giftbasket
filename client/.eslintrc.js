059e82af02d8e61ac3df8695eb8b85b73d759239
melissay94 <melissay94@yahoo.com>
2020-04-22 15:03:00 -0700

    Created build only branch

--- client/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: "airbnb",
-  rules: {
-    "react/jsx-filename-extension": "off",
-    "react/prop-types": "off",
-    "react/jsx-props-no-spreading": "off"
-  }
-}
---
+++ precache-manifest.bf0c11501d10a7deb84fd4110ccec979.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bbc5a5577117a43dbd5eddf5e5abeb",
+    "url": "/giftbasket/index.html"
+  },
+  {
+    "revision": "ef2936aaa923f0002cb8",
+    "url": "/giftbasket/static/css/main.ea9c5ad9.chunk.css"
+  },
+  {
+    "revision": "757273ddfc73f51c8531",
+    "url": "/giftbasket/static/js/2.9e192e01.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/giftbasket/static/js/2.9e192e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2936aaa923f0002cb8",
+    "url": "/giftbasket/static/js/main.1bdbffed.chunk.js"
+  },
+  {
+    "revision": "21c5dc77f0d7a0bd798e",
+    "url": "/giftbasket/static/js/runtime-main.271d1315.js"
+  },
+  {
+    "revision": "acf6e6f5f0d7f11d8d48bf4afdd6770f",
+    "url": "/giftbasket/static/media/GiftBasketLogo.acf6e6f5.png"
+  },
+  {
+    "revision": "6b9a78de564ddbbe458a42f03cc2c365",
+    "url": "/giftbasket/static/media/createIcon.6b9a78de.png"
+  },
+  {
+    "revision": "32d06af8d21141549f2a8ba1855534cc",
+    "url": "/giftbasket/static/media/findIcon.32d06af8.png"
+  },
+  {
+    "revision": "c4413aed591fa6004fbf9eb04c01889b",
+    "url": "/giftbasket/static/media/organizeIcon.c4413aed.png"
+  },
+  {
+    "revision": "3b56106a1094e90393a234a0e1456e9e",
+    "url": "/giftbasket/static/media/shareIcon.3b56106a.png"
+  },
+  {
+    "revision": "b358072c6e244f7830eaa5977f08cef8",
+    "url": "/giftbasket/static/media/stockBackgroundPhoto.b358072c.jpg"
+  }
+]);
